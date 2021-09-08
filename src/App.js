ffe8b13b9a42a1f70af37eeceab28529327935eb
hongdmo2 <47607919+hongdmo2@users.noreply.github.com>
2021-09-07 21:51:58 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="63dffddd-a5c9-4078-808b-0fc94d84584a"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+
+        
+        />
+    );
+}
+export default App;
