ba1bfcf9164ce2c8dfcbd648b1460bd8666b01d6
hongdmo2 <47607919+hongdmo2@users.noreply.github.com>
2021-10-24 05:43:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './App'; //import App.js
 
 ReactDOM.render(<App />, document.getElementById('root')); // in here we render our our entire application.
                                                           // every react application starts with a simple div that's named root and you can see the right in the public, index.html
